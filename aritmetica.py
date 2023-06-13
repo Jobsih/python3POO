38802220d14f503b1815677703d4e245b827933a
Jobsih <60786743+Jobsih@users.noreply.github.com>
2023-06-13 14:05:19 -0600

    Add files via upload

---
+++ aritmetica.py
@@ -0,0 +1,18 @@
+def suma(a,b):
+    if a != None and b!= None:
+        c = a+b
+    else:
+        print('Envía ambos valores')
+        c=0
+    return c
+
+def resta(a=0,b=1):
+    return a-b
+    '''
+    if a != None and b!= None:
+        c = a-b
+    else:
+        print('Envía ambos valores')
+        c=0
+    return c
+'''
---
+++ main.py
@@ -0,0 +1,9 @@
+import  aritmetica as ari
+
+
+def main():
+    resultado  = ari.suma(3,4)
+    print(resultado )
+    print(ari.resta(b=5,a=3))
+
+main()
