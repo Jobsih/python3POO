b72fd8621d47d13e9a7123f9a33dea31ba6e3bf3
Jobsih <jorgeomarsih@gmail.com>
2023-06-15 19:56:30 -0600

    Clase del jueves

--- Jueves/juevesPoo/main.py
+++ Jueves/juevesPoo/main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+from Cosas import *
+def main():
+    l1 = Libro.libro_planeta("El perfume", Autor("Patrick", "PS"), 1980)
+    print(l1)
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    l1.autor.pseudonimo = l1.autor.pseudonimo.lower()
+    print(l1)
 
+    print('----------Herencia----------')
+    al2 = Alumno("José",19,316266285,'ICO',9.5)
+    al2.nombre= "Pedro"
+    print(vars(al2))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+main()
